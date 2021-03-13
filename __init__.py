38c8b2a74b0eb277ebf155439297e22f4b33585f
MarkC-b3d <blendedmarks@gmail.com>
2021-03-13 17:10:02 +0000

    updated version number

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Agile Render",
     "description": "Agile Render Presets",
     "author": "Mark C",
-    "version": (0, 0, 2),
+    "version": (0, 0, 3),
     "blender": (2, 92, 0),
     "location": "Render Panel",
     "warning": "",
